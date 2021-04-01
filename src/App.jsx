c8f091bcffcf8025239ee04331ffc305b9ea18d8
Joao <jp_bast@hotmail.com>
2021-03-31 23:07:34 -0300

    piece together all components to build the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
+import Navbar from './components/Navbar.jsx';
+import Description from './components/Description.jsx';
+import ShortenLink from './components/ShortenLink.jsx';
+import Features from './components/Features.jsx';
+import GetStarted from './components/GetStarted.jsx';
+import Footer from './components/Footer.jsx';
 
 function App() {
   return (
-    
+    <div>
+      <div className="container bg-white">
+        <Navbar />
+        <Description />
+      </div>
+      <div className="container-shorten bg-white-gray">
+        <ShortenLink />
+      </div>
+      <div className="bg-gray">
+        <Features />
+      </div>
+      <GetStarted />
+      <Footer />
+    </div>
+
   );
 }
 
