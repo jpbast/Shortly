0d630e714ab61393a3fc7ee7ddd826ed0af035c1
Joao <jp_bast@hotmail.com>
2021-04-02 14:14:26 -0300

    adding mobile menu interaction

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,57 @@
-import React from 'react';
+/* eslint-disable max-len */
+import React, {useEffect, useState} from 'react';
 import Logo from '../assets/images/logo.svg';
+import '../styles/MenuNavigation.css';
 
 function Navbar() {
+  const [icon, setIcon] = useState(false);
+  const [size, setSize] = useState(1080);
+  const [flag, setFlag] = useState(true);
+
+  window.addEventListener('resize', () => {
+    setSize(window.innerWidth);
+  });
+
+  function handleIcon() {
+    setIcon(!icon);
+  }
+
+  useEffect(() => {
+    if (size > 830 && flag) {
+      setIcon(false);
+      setFlag(false);
+    }
+    if (size <= 830 && !flag) {
+      setFlag(true);
+    }
+
+    if (icon) {
+      document.querySelector('.navbar ul').style.display = 'flex';
+    } else {
+      if (flag) {
+        document.querySelector('.navbar ul').style.display = 'none';
+      } else {
+        document.querySelector('.navbar ul').style.display = 'flex';
+      }
+    }
+  }, [size, icon]);
+
+
   return (
     <div className="navbar">
       <img src={Logo} alt="Logo"/>
-      <ul>
-        <li>Features</li>
-        <li>Pricing</li>
-        <li>Resources</li>
-      </ul>
-      <div className="login-signup">
-        <span>Login</span>
-        <button>Sign Up</button>
+      <div className={icon ? 'mob-nav-container' : 'nav-container'} >
+        <ul>
+          <li>Features</li>
+          <li>Pricing</li>
+          <li>Resources</li>
+        </ul>
+        <div className={icon ? 'login' : 'login-signup'}>
+          <span>Login</span>
+          <button>Sign Up</button>
+        </div>
       </div>
-      <div className="menu-icon-container">
+      <div onClick={handleIcon} className="menu-icon-container">
         <div className="menu-icon"></div>
         <div className="menu-icon"></div>
         <div className="menu-icon"></div>
