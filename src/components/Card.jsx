88a96027fc34e4a1ee0637b046f27a74408512cc
Joao <jp_bast@hotmail.com>
2021-04-01 21:58:38 -0300

    changing cards logo

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,12 +2,28 @@
 /* eslint-disable max-len */
 import React from 'react';
 import brandRecognition from '../assets/images/icon-brand-recognition.svg';
+import detailRecords from '../assets/images/icon-detailed-records.svg';
+import fullyCustomizable from '../assets/images/icon-fully-customizable.svg';
 
 function Card(props) {
+  let image = null;
+  switch (props.title) {
+    case 'Brand Recognition':
+      image = brandRecognition;
+      break;
+    case 'Detailed Records':
+      image = detailRecords;
+      break;
+    case 'Fully Customizable':
+      image = fullyCustomizable;
+    default:
+      break;
+  }
+
   return (
     <div className="container-card">
       <div className="container-card-img">
-        <img src={brandRecognition} alt=""/>
+        <img src={image} alt=""/>
       </div>
       <div className="card-desc">
         <h3 className="title">{props.title}</h3>
