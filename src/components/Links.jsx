4a54b4d478f1459a9d6c15af95325a5fc20755eb
Joao <jp_bast@hotmail.com>
2021-04-05 13:52:29 -0300

    adding responsive links component to mobile

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -13,26 +13,52 @@ function Links(props) {
     padding: 10px 15px 10px 15px;
     display: flex;
     justify-content: space-between;
-    
+    gap: 20px;
+
     &&:not(:last-child) {
         margin-bottom: 20px;
     }
-    && div {
+    && :first-child {
         display: flex;
         width: 100%;
         justify-content: space-between;
         align-items: center;
-        padding-right: 25px;
+        box-sizing: border-box;
     }
-    && div :nth-child(2) {
+    && :first-child :nth-child(2) {
         color: hsl(180, 66%, 49%);
         text-decoration: none;
+    }
+  
+    @media screen and (max-width: 830px) {
+      flex-direction: column;
+      display: flex;
+      gap: 0;
+      padding: 10px 0 10px 0;
+      && :first-child {
+        flex-direction: column;
+        align-items: flex-start;
+      }
+      && div :first-child {
+        width: 100%;
+        border-width: 2px;
+        border-color: rgb(158, 154, 167, 0.3);
+        border-style: none none solid none;
+        padding-bottom: 10px;
+      }
+      && div span {
+        padding-left: 15px;
+      }
+      && :last-child  {
+        padding: 10px 15px 0px 15px;
+        align-items: center;
+      }
     }
     `;
   const copied = {
     backgroundColor: 'hsl(257, 27%, 26%)',
     borderBox: 'box-sizing',
-    padding: '10px 20px 10px 20px',
+    padding: '10px 17px 10px 17px',
     borderRadius: '10px',
   };
 
@@ -58,7 +84,9 @@ function Links(props) {
               <span>{link.link}</span>
               <span id={link.short_link}>{link.short_link}</span>
             </div>
-            <button style={linkCopied == link.short_link ? copied : {borderRadius: '10px'}} onClick={() => copyToClipboard(link.short_link)}>{linkCopied == link.short_link ? 'Copied' : 'Copy'}</button>
+            <div>
+              <button style={linkCopied == link.short_link ? copied : {borderRadius: '10px'}} onClick={() => copyToClipboard(link.short_link)}>{linkCopied == link.short_link ? 'Copied!' : 'Copy'}</button>
+            </div>
           </LinksWrapper>
         );
       })}
